b3718636b08b59da7d710fe2b2c356f121ec8ded
Anastasiia Shaikina <shaikina@phystech.edu>
2022-12-12 21:15:43 +0100

    fix: исправить замечания ревьюера

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,6 +1,12 @@
 import Popup from './Popup.js';
 
 export default class PopupWithImage extends Popup {
+  constructor(popupSelector) {
+    super(popupSelector);
+    this._cardFidcaption = document.querySelector('.popup__fidcaption');
+    this._cardBigImage = document.querySelector('.popup__image');
+  }
+
   open(cardImage, cardTitle) {
     super.open();
 
--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   devServer: {
     static: path.resolve(__dirname, './dist'),
     compress: true,
-    port: 8073,
+    port: 8072,
     open: true
   },
   module: {
